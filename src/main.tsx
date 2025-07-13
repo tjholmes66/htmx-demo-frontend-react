6627491a04afad515ad1ea3a2e6bd4e9880ff9ce
Thomas Joseph Holmes Jr. <tom@tomholmes.net>
2025-07-13 16:01:55 -0400

    New Commit after adding Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from '@mui/material';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.tsx";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+    <React.StrictMode>
+        <CssBaseline />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
