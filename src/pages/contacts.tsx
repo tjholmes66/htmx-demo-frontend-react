d2f1de3ddbc940a5039038db8f049809f2321be6
Thomas Joseph Holmes Jr. <tom@tomholmes.net>
2025-07-29 14:45:42 -0400

    created new Links Grid and updates with correct data from contract grid

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,7 +1,8 @@
 import ContactsDataGrid from '../components/grids/ContactsDataGrid.tsx';
 import ContactEmailsDataGrid from "../components/grids/ContactEmailsDataGrid.tsx";
 import {ContactContextProvider} from "../contexts/contact-context.tsx";
-import ContactLinksDataGrid from "../components/grids/ContactLinksDataGrid.tsx";
+import ContactLinksDataGrid from "../components/grids/ContactLinksDataGrid.tsx"
+import { Grid } from '@mui/material';
 
 export const ContactsPage = () => {
 
@@ -9,11 +10,20 @@ export const ContactsPage = () => {
     <>
         <ContactContextProvider >
             <ContactsDataGrid/>
-            <ContactEmailsDataGrid/>
-            <ContactLinksDataGrid/>
+            <Grid container spacing={2}> {/* Use Grid container for layout */}
+                <Grid size={{ xs: 12, md: 6 }}> {/* First Grid item, takes 6 columns on medium screens and up */}
+                    <div style={{ height: 400, width: '100%' }}>
+                            <ContactEmailsDataGrid/>
+                    </div>
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}> {/* Second Grid item, takes 6 columns on medium screens and up */}
+                    <div style={{ height: 400, width: '100%' }}>
+                            <ContactLinksDataGrid/>
+                    </div>
+                </Grid>
+            </Grid>
         </ContactContextProvider>
     </>
-
   );
 
 }
